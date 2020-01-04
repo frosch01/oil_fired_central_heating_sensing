b88a299f66a757f9a6a3d35e8ba7c3a3f8546ca7
Ralph Scharpf <ralph.scharpf@gmail.com>
2020-01-04 15:13:02 +0100

    ADDED: Tests for updating events

--- test_event_collect_recorder.py
+++ test_event_collect_recorder.py
@@ -23,7 +23,7 @@ def test_emtpy_positions(exec):
     """
     rec = EventCollectRecorder("./test.txt")
     rec.register_event_source("SRC10", 10, "test10")
-    line = rec.format_event(rec.event_map)
+    line = rec.format_event(rec.event_head)
     exec.report(line == "0 test10", "Event formatting")    
     
 def test_position_ordering(exec):
@@ -35,7 +35,7 @@ def test_position_ordering(exec):
     rec.register_event_source("SRC3", 3, "test3")
     rec.register_event_source("SRC2", 2, "test2")
     rec.register_event_source("SRC1", 1, "test1")
-    line = rec.format_event(rec.event_map)
+    line = rec.format_event(rec.event_head)
     exec.report(line == "0 test1 test2 test3", "Output position")
     
 def test_event_ordered(exec):
@@ -44,9 +44,12 @@ def test_event_ordered(exec):
     rec.register_event_source("SRC1", 1, "init1")
     rec.register_event_source("SRC2", 2, "init2")
     rec.register_event_source("SRC3", 3, "init3")
-    rec.create_event("SRC1", 1.0, "update1")
-    rec.create_event("SRC2", 1.5, "update2")
-    rec.create_event("SRC3", 2.0, "update3")
+    # Try to create an event for an unregistered source
+    exec.call_except(lambda: rec.create_event("SRC4", 1.0, "update4"), Exception)
+    # Check expected usage
+    rec.create_event("SRC1", 1.0, "event1")
+    rec.create_event("SRC2", 1.5, "event2")
+    rec.create_event("SRC3", 2.0, "event3")
     queue_len = len(rec.event_queue)
     exec.report(queue_len == 3, "Event in queue ({}) == 3".format(queue_len))
     time_expected=[1.0, 1.5, 2.0]
@@ -54,34 +57,72 @@ def test_event_ordered(exec):
         exec.report(event["Time"] == expected, "Events are stored in timely order")
 
 def test_event_unordered(exec):
-    """Test event creation and caching with timely out of order events"""
+    """Test event creation and caching with timely out of order events.
+    Updates done before head are expected to be propagated to the head
+    """
     rec = EventCollectRecorder("./test.txt", 2)
     rec.register_event_source("SRC1", 1, "init1")
     rec.register_event_source("SRC2", 2, "init2")
     rec.register_event_source("SRC3", 3, "init3")
-    rec.create_event("SRC1", 2.0, "update1")
-    rec.create_event("SRC2", 1.0, "update2")
-    rec.create_event("SRC3", 1.5, "update3")
-    queue_len = len(rec.event_queue)
-    exec.report(queue_len == 3, "Event in queue ({}) == 3".format(queue_len))
-    time_expected=[1.0, 1.5, 2.0]
-    for event, expected in zip(rec.event_queue, time_expected):
-        exec.report(event["Time"] == expected, "Events are stored in timely order")
+    rec.create_event("SRC1", 2.0, "event1")
+    rec.create_event("SRC2", 1.0, "event2")
+    expected = [{'Time':1.0, 'SRC1':'init1',  'SRC2':'event2', 'SRC3':'init3'}, 
+                {'Time':2.0, 'SRC1':'event1', 'SRC2':'event2', 'SRC3':'init3'}]
+    exec.report(rec.event_queue == expected, "Expected queue after inserting before head")
+    rec.create_event("SRC3", 1.5, "event3")
+    expected = [{'Time':1.0, 'SRC1':'init1',  'SRC2':'event2', 'SRC3':'init3'}, 
+                {'Time':1.5, 'SRC1':'init1',  'SRC2':'event2', 'SRC3':'event3'},
+                {'Time':2.0, 'SRC1':'event1', 'SRC2':'event2', 'SRC3':'event3'}]
+    exec.report(rec.event_queue == expected, "Expected queue after inserting in between")
+    rec.create_event("SRC1", 2.5, "event1_1")
+    expected = [{'Time':1.0, 'SRC1':'init1',    'SRC2':'event2', 'SRC3':'init3'}, 
+                {'Time':1.5, 'SRC1':'init1',    'SRC2':'event2', 'SRC3':'event3'},
+                {'Time':2.0, 'SRC1':'event1',   'SRC2':'event2', 'SRC3':'event3'},
+                {'Time':2.5, 'SRC1':'event1_1', 'SRC2':'event2', 'SRC3':'event3'}]
+    exec.report(rec.event_queue == expected, "Expected queue after inserting at head")
         
 def test_dump_on_time_exceed(exec):
     """Test event dump/cache removal as storage time exceeds"""
     rec = EventCollectRecorder("./test.txt", 2)
     rec.register_event_source("SRC1", 1, "init1")
     rec.register_event_source("SRC2", 2, "init2")
-    rec.create_event("SRC1", 1.0, "update1_1")
-    rec.create_event("SRC2", 2.0, "update2_1")
-    rec.create_event("SRC1", 3.0, "update1_2")
-    rec.create_event("SRC2", 4.1, "update2_2")
+    rec.create_event("SRC1", 1.0, "event1_1")
+    rec.create_event("SRC2", 2.0, "event2_1")
+    rec.create_event("SRC1", 3.0, "event1_2")
+    rec.create_event("SRC2", 4.1, "event2_2")
     queue_len = len(rec.event_queue)
     exec.report(queue_len == 2, "Event in queue ({}) == 2".format(queue_len))
     time_expected=[3.0, 4.1]
     for event, expected in zip(rec.event_queue, time_expected):
         exec.report(event["Time"] == expected, "Events are stored in timely order")
+        
+def test_update_event(exec):
+    """Test for updating events in the cache
+    Updates before are expected to be propagated towards head as long as there 
+    has not been stored another event of same source
+    """
+    rec = EventCollectRecorder("./test.txt", 2)
+    rec.register_event_source("SRC1", 1, "init1")
+    rec.register_event_source("SRC2", 2, "init2")
+    rec.register_event_source("SRC3", 3, "init3")
+    # Add events and check content
+    rec.create_event("SRC1", 1.0, "event1")
+    rec.create_event("SRC2", 2.0, "event2")
+    rec.create_event("SRC3", 3.0, "event3")
+    expected = [{'Time':1.0, 'SRC1':'event1', 'SRC2':'init2',  'SRC3':'init3'}, 
+                {'Time':2.0, 'SRC1':'event1', 'SRC2':'event2', 'SRC3':'init3'},
+                {'Time':3.0, 'SRC1':'event1', 'SRC2':'event2', 'SRC3':'event3'}]
+    exec.report(rec.event_queue == expected, "Expected queue content before update")
+    rec.create_event("SRC2", 1.0, "update2")
+    expected[0]["SRC2"] = "update2"
+    exec.report(rec.event_queue == expected, "Expected update without propagation of SRC2 at time 1.0")
+    rec.create_event("SRC1", 2.0, "update1")
+    expected[1]["SRC1"] = "update1"
+    expected[2]["SRC1"] = "update1"
+    exec.report(rec.event_queue == expected, "Expected propagation after update of SRC1 at time 2.0")
+    rec.create_event("SRC3", 3.0, "update3")
+    expected[2]["SRC3"] = "update3"
+    exec.report(rec.event_queue == expected, "Expected update of last queue element at time 3.0")
 
 if __name__== "__main__":
     TestExec(test_registration_pos_0).execute()
@@ -91,4 +132,5 @@ if __name__== "__main__":
     TestExec(test_event_ordered).execute()    
     TestExec(test_event_unordered).execute()    
     TestExec(test_dump_on_time_exceed).execute()
+    TestExec(test_update_event).execute()
     
